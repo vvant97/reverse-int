1958b940e92bb51a7693e1fbea7ae90bfcd7f2c0
Vladyslav Tyshchenko <vvant97@gmail.com>
2022-06-21 12:54:15 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
   
-}
+  let numberToString = String(n);
+  let string = '';
+
+  for (let i = numberToString.length - 1; i >= 0; i--) {
+    string += numberToString[i];
+  }
+
+  if (n < 0) {
+    string = string.replace(string[string.length - 1], '');
+  }
+  
+  return Number(string);
+
+};
